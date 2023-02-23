28e2094fb469afcf99d73055c8252177b6c49476
Prantadas7 <pranta_das@coredevs.ltd>
2023-02-24 00:17:27 +0600

    build file added

---
+++ lib/cjs/utils/urls.d.ts
@@ -0,0 +1,2 @@
+export declare function getBaseUrl(useFuturesTestnet: boolean, useSpotTestnet: boolean): string;
+export declare function getWebsocketUrl(useFuturesTestnet: boolean, useSpotTestnet: boolean): string;
---
+++ lib/esm/utils/fetchService.d.ts
@@ -0,0 +1,1 @@
+export function fetchCandleStickData(symbol: string | undefined, interval: string | undefined, useFuturesTestnet: any, useSpotTestnet: any): Promise<any>;
